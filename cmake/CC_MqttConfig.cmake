4cc36fd0686b9698eb9ec765089f578564a72dcd
Alex Robenko <arobenko@gmail.com>
2017-03-23 17:01:40 +1000

    Fixing cmake config file.

--- cmake/CC_MqttConfig.cmake
+++ cmake/CC_MqttConfig.cmake
@@ -5,7 +5,9 @@
 #  CC_MQTT_FOUND - Found MQTT protocol library, use MQTT_INCLUDE_DIRS for includes
 #  CC_MQTT_INCLUDE_DIRS - Where to find all the headers
 
-get_filename_component (MQTT_INSTALL_PATH ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+get_filename_component (MQTT_INSTALL_LIB_PROJ_DIR ${MQTT_INSTALL_LIB_PROJ_DIR} DIRECTORY)
+get_filename_component (MQTT_INSTALL_LIB_DIR ${CC_INSTALL_LIB_PROJ_DIR} DIRECTORY)
+get_filename_component (MQTT_INSTALL_PATH ${MQTT_INSTALL_LIB_DIR} DIRECTORY)
 
 find_path(CC_MQTT_INCLUDE_DIR NAMES mqtt/protocol/Message.h PATHS "${MQTT_INSTALL_PATH}" PATH_SUFFIXES include)
 
